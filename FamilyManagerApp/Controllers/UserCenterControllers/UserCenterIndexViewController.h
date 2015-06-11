858d9d0dc9efe836e8f8cc49945d2341f0ffdbdd
Provence <305213781@qq.com>
2015-06-11 16:09:08 +0800

    初步设计个人信息页面

---
+++ FamilyManagerApp/Controllers/UserCenterControllers/UserCenterIndexViewController.h
@@ -0,0 +1,15 @@
+//
+//  UserCenterIndexViewController.h
+//  FamilyManagerApp
+//
+//  Created by ESI on 15/6/11.
+//  Copyright (c) 2015年 LYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserCenterIndexViewController : UITableViewController
+@property (weak, nonatomic) IBOutlet UILabel *lblAccount;
+@property (weak, nonatomic) IBOutlet UILabel *lblName;
+
+@end
---
+++ FamilyManagerApp/Controllers/UserCenterControllers/UserCenterIndexViewController.m
@@ -0,0 +1,116 @@
+//
+//  UserCenterIndexViewController.m
+//  FamilyManagerApp
+//
+//  Created by ESI on 15/6/11.
+//  Copyright (c) 2015年 LYC. All rights reserved.
+//
+
+#import "UserCenterIndexViewController.h"
+
+@interface UserCenterIndexViewController ()
+
+@end
+
+@implementation UserCenterIndexViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    self.lblAccount.text = @"10000";
+    self.lblName.text = @"刘一呈";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    if (section == 0) {
+        return 1;
+    }
+    else if (section == 1)
+    {
+        return 3;
+    }
+    return 1;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+
+    return 10;
+}
+
+@end
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
