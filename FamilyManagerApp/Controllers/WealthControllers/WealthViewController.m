1fc6883998b23b7a6cb3848cfe5f9aff3da6d9eb
Provence <305213781@qq.com>
2015-07-02 14:49:36 +0800

    总资产查询功能。

--- FamilyManagerApp/Controllers/WealthControllers/WealthViewController.m
+++ FamilyManagerApp/Controllers/WealthControllers/WealthViewController.m
@@ -10,6 +10,11 @@
 #import "LycScrollCollectionViewCell.h"
 #import "AppConfiguration.h"
 
+#import "AppDelegate.h"
+#import "LycDialogView.h"
+#import "ASIFormDataRequest.h"
+#import "ApiJsonHelper.h"
+
 @interface WealthViewController ()
 {
     NSArray *collectionData;
@@ -23,6 +28,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    _lycDialog = [[LycDialogView alloc] initWithTitle:@"正在加载" andSuperView:self.view isModal:NO];
     
     NSDictionary *dic1 = [NSDictionary dictionaryWithObjects:@[@"标题11111111",@"dog.png"] forKeys:@[@"title",@"img"]];
     NSDictionary *dic2 = [NSDictionary dictionaryWithObjects:@[@"标题22222222",@"duck.png"] forKeys:@[@"title",@"img"]];
@@ -38,11 +45,49 @@
     self.automaticallyAdjustsScrollViewInsets = NO;
     
     [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForItem:3000 inSection:0] atScrollPosition:UICollectionViewScrollPositionLeft animated:NO];
+    
+    [self loadCaichan];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)loadCaichan
+{
+    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+    BOOL isLogin = [ud boolForKey:__fm_defaultsKey_loginUser_Status];
+    NSString *serverIP = __fm_userDefaults_serverIP;
+    if (self.appDelegate.isConnectNet == YES && isLogin == YES) {
+        [self.lycDialog showDialog:nil];
+        //第1个任务，下载用户银行信息
+        ASIFormDataRequest *requestUB= [ASIFormDataRequest requestWithURL:[NSURL URLWithString:[serverIP stringByAppendingString:__fm_apiPath_queryZiChan]]];
+        __weak ASIFormDataRequest *request_weak = requestUB;//使用block回调函数时，要使用弱指针对象调用，不然会导致循环引用，ASI对象无法释放，导致内存泄漏
+        _test_request = requestUB;
+        requestUB.shouldAttemptPersistentConnection = YES;
+        requestUB.requestMethod = @"POST";
+        NSInteger userID = [ud integerForKey:__fm_defaultsKey_loginUser_ID];
+        [requestUB addPostValue:[NSNumber numberWithInteger:userID] forKey:@"userid"];
+        [requestUB setFailedBlock:^{
+            [self.lycDialog hideDialog];
+        }];
+        [requestUB setCompletionBlock:^{
+            NSData *data = [request_weak responseData];
+            ApiJsonHelper *aj = [[ApiJsonHelper alloc] initWithData:data requestName:@"获取资产"];
+            if (aj.bSuccess == YES) {
+                NSString *cashMoney = [aj.jsonObj objectForKey:@"cashMoney"];
+                NSString *bankMoney = [aj.jsonObj objectForKey:@"bankMoney"];
+                NSString *totalMoney = [aj.jsonObj objectForKey:@"totalMoney"];
+                self.lblCash.text = [NSString stringWithFormat:@"¥ %@", cashMoney];
+                self.lblBank.text = [NSString stringWithFormat:@"¥ %@", bankMoney];
+                self.lblTotal.text = [NSString stringWithFormat:@"¥ %@", totalMoney];
+            }
+            [self.lycDialog hideDialog];
+        }];
+        [requestUB startAsynchronous];
+    }
+
 }
 
 /*
@@ -55,6 +100,7 @@
 }
 */
 
+/**************UICollectionView 委托**************/
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
 {
     return 1;
@@ -80,4 +126,9 @@
     [cell setContents:dic[@"title"] andImage:img];
     return cell;
 }
+//刷新tap事件
+- (IBAction)refresh_click:(id)sender {
+    [self loadCaichan];
+}
+
 @end
